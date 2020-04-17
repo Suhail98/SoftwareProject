9b40590e50423ec4b9efca90d9904c51d2c9ff46
Abdelrahman Abdallah Salah <36174099+Argendi@users.noreply.github.com>
2020-04-18 00:11:06 +0200

    Check if listUsers called by Administrator

--- Controllers/RegisteredUsersController.cs
+++ Controllers/RegisteredUsersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,6 +10,7 @@ namespace UserPackage
 {
     public class RegisteredUsersController : ApiController, IUserFinder, IListAllUsers
     {
+        static private SqlConnection conn = new SqlConnection("Data Source=sql5053.site4now.net;Initial Catalog=DB_A5A92A_SWEDB;Persist Security Info=True;User ID=DB_A5A92A_SWEDB_admin;Password=aaas2020");
         private ISearchUserRepo[] searchUserRepos = new ISearchUserRepo[]{ new AdminstratorDataBase(), new NormalUserDataBase(), new StoreOwnerDataBase() };
         private IListUserRepo[] listUserRepos = new IListUserRepo[] {new NormalUserDataBase(), new StoreOwnerDataBase() };
         
@@ -26,10 +28,13 @@ namespace UserPackage
         [HttpGet]
         public List<User> listAllUsers()
         {
+            if (Global.getCurrentSession() != null && Global.getCurrentSession().GetType() != typeof(AdminstratorController))   return null;
+
             List<User> clientUsers = new List<User>();
             for (int i = 0; i < listUserRepos.Length; i++)
                 clientUsers.AddRange(listUserRepos[i].listUsers());
             return clientUsers;
+
         }
     }
 }
