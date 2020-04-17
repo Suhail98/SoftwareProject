21a909ac77d32288a97b77d96dc5d45f4a88fac5
Abdelrahman Abdallah Salah <36174099+Argendi@users.noreply.github.com>
2020-04-17 23:46:05 +0200

    Adding Administrator feature in AdminstratorDataBase class

--- Models/AdminstratorDataBase.cs
+++ Models/AdminstratorDataBase.cs
@@ -12,7 +12,7 @@ namespace UserPackage {
 
     public class AdminstratorDataBase : IAddUserRepo, IUpdateUserRepo, IDeleteUserRepo, IListUserRepo,ISearchUserRepo
 {
-
+        static private SqlConnection conn = new SqlConnection("Data Source=sql5053.site4now.net;Initial Catalog=DB_A5A92A_SWEDB;Persist Security Info=True;User ID=DB_A5A92A_SWEDB_admin;Password=aaas2020");
         List<Adminstrator> AdminUser = new List<Adminstrator>
         {
             new Adminstrator("suhail@gmail.com","suhail","456"),
@@ -32,12 +32,14 @@ namespace UserPackage {
         /// <returns>
         /// </returns>
         public void addUser(User user)
-        {
-            
-    // section -87--2-87--42--4267bcd9:1710d3b46a2:-8000:000000000000098D begin
-    // section -87--2-87--42--4267bcd9:1710d3b46a2:-8000:000000000000098D end
-
-    }
+        {     
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "insert into Adminstrator values('" + user.email + "','" + user.userName + "','" + user.password + "')";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
 
         /// <summary>
         ///  An operation that does...
