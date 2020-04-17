6d3056ea4866f03b9ca34e2cafc2841a01420a48
abdelraahman <43926581+abdelraahman@users.noreply.github.com>
2020-04-18 01:34:36 +0200

    Update OnlineStore DataBase

--- Models/OnlineStoreDataBase.cs
+++ Models/OnlineStoreDataBase.cs
@@ -1,87 +1,65 @@
-// FILE: D:/New Folder (22)/StorePackage/OnlineStoreDataBase.cs
+using System.Data.SqlClient;
+using UserPackage;
 
-// In this section you can add your own using directives
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000AAD begin
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000AAD end
-namespace StorePackage {
-
-/// <summary>
-    ///  A class that represents ...
-    /// 
-    ///  @see OtherClasses
-    ///  @author your_name_here
-     /// </summary>
-public class OnlineStoreDataBase: IAddStoreRepo, IUpdateStoreRepo, IDeleteStoreRepo, ISearchStoreRepo
+namespace StorePackage
 {
-    // Operations
 
-    public /// <summary>
-    ///  An operation that does...
-    /// 
-    ///  @param firstParam a description of this parameter
-    /// </summary>
-    /// <param name="OnlineStore">
-    /// </param>
-    /// <returns>
-    /// </returns>
-     void addStore(Store store)
+
+    public class OnlineStoreDataBase : IAddStoreRepo, IUpdateStoreRepo, IDeleteStoreRepo, ISearchStoreRepo
     {
-            
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B04 begin
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B04 end
+        static private SqlConnection conn = new SqlConnection("Data Source=sql5053.site4now.net;Initial Catalog=DB_A5A92A_SWEDB;Persist Security Info=True;User ID=DB_A5A92A_SWEDB_admin;Password=aaas2020");
+        // Operations
 
-    }
+        public void addStore(Store store)
+        {
 
-    public /// <summary>
-    ///  An operation that does...
-    /// 
-    ///  @param firstParam a description of this parameter
-    /// </summary>
-    /// <param name="String">
-    /// </param>
-    /// <returns>
-    /// </returns>
-     Store searchStoreByName( string a)
-    {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "insert into OnlineStore  values('" + store.getStoreName() + "','" + store.getStoreAddress() + "','" + store.getStoreOwner().email + "')";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        public Store searchStoreByName(string name)
+        {
+            conn.Open();
+            string strSelect = "Select * From OnlineStore where storeName=@name";
+            SqlCommand cmd = new SqlCommand(strSelect, conn);
+            cmd.Parameters.Add("@name", name);
+            SqlDataReader myReader = cmd.ExecuteReader();
+            if (myReader.Read())
+            {
+                string storename = myReader["storeName"].ToString();
+                string storeaddress = myReader["storeAddress"].ToString();
+                string storeowner = myReader["storeOwner"].ToString();
+                StoreOwnerDataBase sodb = new StoreOwnerDataBase();
+                StoreOwnerController storeOwner = (StoreOwnerController)sodb.searchByEmail(storeowner);
+                Store store = new Store(storename, storeaddress, (StoreOwner)storeOwner.getModel());
+                conn.Close();
+                return store;
+            }
+            conn.Close();
             return null;
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B07 begin
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B07 end
 
-    }
+        }
 
-    public /// <summary>
-    ///  An operation that does...
-    /// 
-    ///  @param firstParam a description of this parameter
-    /// </summary>
-    /// <param name="OnlineStore">
-    /// </param>
-    /// <returns>
-    /// </returns>
-     void updateStore(Store a)
-    {
-          //  return false;
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0A begin
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0A end
+        public void updateStore(Store a)
+        {
+            //  return false;
+            // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0A begin
+            // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0A end
 
-    }
+        }
 
-    public /// <summary>
-    ///  An operation that does...
-    /// 
-    ///  @param firstParam a description of this parameter
-    /// </summary>
-    /// <param name="String">
-    /// </param>
-    /// <returns>
-    /// </returns>
-     void deleteStoreByName(string a)
-    {
+        public void deleteStoreByName(string a)
+        {
             //return false;
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0D begin
-    // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0D end
+            // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0D begin
+            // section -87--2-87--42--46e34135:1711ce644e4:-8000:0000000000000B0D end
 
+        }
     }
-} /* end class OnlineStoreDataBase */
 
 }
